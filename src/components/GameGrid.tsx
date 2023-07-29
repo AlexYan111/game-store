ad1b2b54202b4d3f0f67a5f891c56d9b0952575c
Alex <wandong_yan@brown.edu>
2023-07-28 22:48:55 -0400

    Display Critic Score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,8 +10,8 @@ const GameGrid = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing="20px"
-        padding="10px"
+        spacing="40px"
+        padding="20px"
       >
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
