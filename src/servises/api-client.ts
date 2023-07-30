93a8d61f449a00c8bd8c7921abfc4d699999e6fd
Alex <wandong_yan@brown.edu>
2023-07-29 22:47:38 -0400

    Fetch the genres

--- src/servises/api-client.ts
+++ src/servises/api-client.ts
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     // key: "c7b18323a47d40c394ea5b019646b1f5",
-    key: "f3f42349a2874c43a3813801e9556453",
+    key: "9100dc274c8a48a9b014f52a73ffaa42",
   },
 });
