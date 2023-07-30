584e004b10752a3ed1613411283bf68a68651a61
Alex <wandong_yan@brown.edu>
2023-07-30 15:19:19 -0400

    Filter games by genre

--- src/servises/api-client.ts
+++ src/servises/api-client.ts
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     // key: "c7b18323a47d40c394ea5b019646b1f5",
-    key: "9100dc274c8a48a9b014f52a73ffaa42",
+    key: "09c55ec22b3541ae9ff79ff6b04fe1a1",
   },
 });
