e0d3130ad3a474413f33219ef47ff749e8885219
Alex <wandong_yan@brown.edu>
2023-08-13 22:46:41 -0400

    Refactoring- simplify time calculations with ms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
@@ -15,7 +16,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours our data will be refreshed
+    staleTime: ms("24h"), // 24 hours our data will be refreshed
     initialData: platforms, // since we need to make the type of the initial data the same as the type of the data returned by the queryFn, FetchResponse<Platform>
   });
 
